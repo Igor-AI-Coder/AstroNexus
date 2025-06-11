09df9bd0c8c64ea5be42deaeca70ba324f2e94d1
emanoel <eoemanoel@gmail.com>
2025-06-11 18:14:11 -0300

    vitejs dor de cabeça

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/AstroNexus/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        produtos: "produtos.html",
+        carrinho: "carrinho.html",
+        fatosCuriosos: "fatos-curiosos.html",
+        galeria: "galeria.html",
+        planetas: "planetas.html",
+        // adicione outras páginas aqui
+      },
+    },
+  },
 });
